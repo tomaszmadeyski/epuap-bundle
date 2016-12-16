1f7d93ad36e4403d2e835b87b64c69f03138895d
Tomasz Madeyski <tomasz.madeyski@gmail.com>
2016-12-16 13:17:21 +0100

    Adding travis intergration and dummy test

---
+++ Tests/DummyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Madeyski\EpuapBundle\Tests;
+
+
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_aaa()
+    {
+        $this->assertTrue(true);
+    }
+}
